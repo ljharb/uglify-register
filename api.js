37bb957f093594bcb8b13624151574044f4edb88
Jordan Harband <ljharb@gmail.com>
2017-07-20 21:20:06 -0700

    Implementation.

---
+++ api.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var extensions = require.extensions;
+var orig = {};
+var minify = require('uglify-js').minify;
+
+module.exports = {
+	register: function register(options) {
+		var opts = options || {};
+		var exts = opts.extensions || ['.js'];
+		var exclude = typeof opts.exclude === 'undefined' ? [] : opts.exclude;
+		var uglifyOptions = opts.uglify || {};
+
+		if (!Array.isArray(exts)) {
+			throw new TypeError('"extensions" option must be an array if provided');
+		}
+		if (exts.length <= 0) {
+			throw new RangeError('"extensions" option, if provided, must not be empty');
+		}
+		if (!Array.isArray(exclude)) {
+			throw new TypeError('"exclude" option must be an array if provided');
+		}
+
+		exts.forEach(function (ext) {
+			if (!orig[ext]) {
+				orig[ext] = extensions[ext];
+			}
+			extensions[ext] = function uglifyRegister(module, filename) {
+				var isExcluded = exclude.some(function (x) {
+					return x.test(filename);
+				});
+				if (!isExcluded) {
+					/* eslint no-underscore-dangle: 0 */
+					var oldCompile = module._compile;
+					module._compile = function (code, file) {
+						var newCode = minify(code, uglifyOptions).code;
+						module._compile = oldCompile;
+						module._compile(newCode, file);
+					};
+					/* eslint no-underscore-dangle: 1 */
+				}
+				return orig[ext](module, filename);
+			};
+		});
+	},
+	unregister: function unregister(options) {
+		var opts = options || {};
+		var exts = opts.extensions || ['.js'];
+		exts.forEach(function (ext) {
+			if (orig[ext]) {
+				extensions[ext] = orig[ext];
+				delete orig[ext];
+			}
+		});
+	},
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var api = require('./api');
+
+api.register();
+
+module.exports = api;
